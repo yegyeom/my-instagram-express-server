d96d8890ef30c23b22e00ff038115e3c0cdf6c3c
yegyeom <reallyy1111@gmail.com>
2021-12-12 03:05:29 +0900

    [feat] 홈 화면 게시물 기능 구현

--- routes/post.js
+++ routes/post.js
@@ -3,22 +3,17 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-const { Post, Hashtag, Image } = require('../models');
+const { Post, Hashtag, Image, User } = require('../models');
 const { isLoggedIn } = require('./middlewares');
+const { sequelize } = require('../models/user');
+const { substring } = require('sequelize/dist/lib/operators');
 
 const router = express.Router();
-
-try {
-    fs.readdirSync('uploads');
-} catch (error) {
-    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
-    fs.mkdirSync('uploads');
-}
 
 const upload = multer({
     storage: multer.diskStorage({
         destination(req, file, cb) {
-            cb(null, 'uploads/');
+            cb(null, '/home/web12/public_html/uploads/');
         },
         filename(req, file, cb) {
             const ext = path.extname(file.originalname);
@@ -29,15 +24,21 @@ const upload = multer({
 });
 
 router.post('/img', isLoggedIn, upload.array('image', 5), (req, res) => {
-    console.log(req.files);
-    res.send(req.files);
+    const { files } = req;
+    files.forEach((file) => {
+        fs.chmodSync(file.path, 0o755);
+        file.path = file.path.substring(23, file.path.length);
+    });
+    res.send(files);
 });
 
 router.post('/', isLoggedIn, async (req, res, next) => {
     try {
+        console.log(req.user);
         const post = await Post.create({
             content: req.body.content,
-            UserId: req.user.id
+            UserId: req.user.id,
+            nick: req.user.userId
         });
         for (let i = 0; i < req.body.imagePaths.length; i++) {
             await Image.create({
@@ -59,6 +60,22 @@ router.post('/', isLoggedIn, async (req, res, next) => {
         }
         console.log('업로드 성공!');
         res.status(200).send('OK');
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+router.get('/posts', isLoggedIn, async (req, res, next) => {
+    try {
+        const posts = await Post.findAll({
+            include: [
+                {
+                    model: Image,
+                }
+            ]
+        });
+        res.status(200).send(posts);
     } catch (error) {
         console.error(error);
         next(error);
